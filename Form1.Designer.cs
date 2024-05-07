3e30a4f23eebf879c4bd6bc977a167f121a85f81
PC <PC@DESKTOP-S31ELKF>
2024-05-07 18:52:34 +0300

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,336 @@
+﻿namespace Form_Test_Fail
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Brand = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.Model = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.FuelBox = new System.Windows.Forms.ComboBox();
+            this.YearBox = new System.Windows.Forms.ComboBox();
+            this.dataGridViewCars = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.Add = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewCars)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Brand
+            // 
+            this.Brand.BackColor = System.Drawing.Color.Aqua;
+            this.Brand.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Brand.Location = new System.Drawing.Point(114, 35);
+            this.Brand.Name = "Brand";
+            this.Brand.Size = new System.Drawing.Size(217, 31);
+            this.Brand.TabIndex = 0;
+            this.Brand.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(22, 32);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(86, 31);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Brand";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(371, 35);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(71, 31);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Year";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(22, 99);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(87, 31);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Model";
+            // 
+            // Model
+            // 
+            this.Model.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Model.Location = new System.Drawing.Point(114, 102);
+            this.Model.Name = "Model";
+            this.Model.Size = new System.Drawing.Size(217, 31);
+            this.Model.TabIndex = 5;
+            this.Model.TextChanged += new System.EventHandler(this.Model_TextChanged);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(371, 99);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(67, 31);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Fuel";
+            this.label5.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // FuelBox
+            // 
+            this.FuelBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FuelBox.FormattingEnabled = true;
+            this.FuelBox.Items.AddRange(new object[] {
+            "Gasoline",
+            "Diesel",
+            "Compressed Natural Gas (CNG)",
+            "Liquefied Petroleum Gas (LPG)",
+            "Ethanol",
+            "Biodiesel",
+            "Hydrogen",
+            "Electricity"});
+            this.FuelBox.Location = new System.Drawing.Point(463, 99);
+            this.FuelBox.Name = "FuelBox";
+            this.FuelBox.Size = new System.Drawing.Size(278, 33);
+            this.FuelBox.TabIndex = 9;
+            // 
+            // YearBox
+            // 
+            this.YearBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.YearBox.FormattingEnabled = true;
+            this.YearBox.Items.AddRange(new object[] {
+            "2024",
+            "2023",
+            "2022",
+            "2021",
+            "2020",
+            "2019",
+            "2018",
+            "2017",
+            "2016",
+            "2015",
+            "2014",
+            "2013",
+            "2012",
+            "2011",
+            "2010",
+            "2009",
+            "2008",
+            "2007",
+            "2006",
+            "2005",
+            "2004",
+            "2003",
+            "2002",
+            "2001",
+            "2000",
+            "1999",
+            "1998",
+            "1997",
+            "1996",
+            "1995",
+            "1994",
+            "1993",
+            "1992",
+            "1991",
+            "1990",
+            "1989",
+            "1988",
+            "1987",
+            "1986",
+            "1985",
+            "1984",
+            "1983",
+            "1982",
+            "1981",
+            "1980",
+            "1979",
+            "1978",
+            "1977",
+            "1976",
+            "1975",
+            "1974",
+            "1973",
+            "1972",
+            "1971",
+            "1970",
+            "1969",
+            "1968",
+            "1967",
+            "1966",
+            "1965",
+            "1964",
+            "1963",
+            "1962",
+            "1961",
+            "1960",
+            "1959",
+            "1958",
+            "1957",
+            "1956",
+            "1955",
+            "1954",
+            "1953",
+            "1952",
+            "1951",
+            "1950"});
+            this.YearBox.Location = new System.Drawing.Point(463, 35);
+            this.YearBox.Name = "YearBox";
+            this.YearBox.Size = new System.Drawing.Size(168, 33);
+            this.YearBox.TabIndex = 10;
+            this.YearBox.SelectedIndexChanged += new System.EventHandler(this.YearBox_SelectedIndexChanged);
+            // 
+            // dataGridViewCars
+            // 
+            this.dataGridViewCars.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewCars.Location = new System.Drawing.Point(12, 354);
+            this.dataGridViewCars.Name = "dataGridViewCars";
+            this.dataGridViewCars.Size = new System.Drawing.Size(753, 193);
+            this.dataGridViewCars.TabIndex = 11;
+            this.dataGridViewCars.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridViewCars_CellClick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(34, 291);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(130, 31);
+            this.label1.TabIndex = 12;
+            this.label1.Text = "Cars List:";
+            this.label1.Click += new System.EventHandler(this.label1_Click_1);
+            // 
+            // Add
+            // 
+            this.Add.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Add.Location = new System.Drawing.Point(54, 183);
+            this.Add.Name = "Add";
+            this.Add.Size = new System.Drawing.Size(118, 41);
+            this.Add.TabIndex = 13;
+            this.Add.Text = "Add";
+            this.Add.UseVisualStyleBackColor = true;
+            this.Add.Click += new System.EventHandler(this.Add_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(195, 183);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(162, 41);
+            this.button2.TabIndex = 14;
+            this.button2.Text = "Update Info";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(386, 183);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(112, 41);
+            this.button3.TabIndex = 15;
+            this.button3.Text = "Remove";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button4.Location = new System.Drawing.Point(656, 281);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(118, 41);
+            this.button4.TabIndex = 16;
+            this.button4.Text = "Exit";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(178, 297);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(24, 25);
+            this.label6.TabIndex = 17;
+            this.label6.Text = "0";
+            this.label6.Click += new System.EventHandler(this.label6_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 589);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.Add);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dataGridViewCars);
+            this.Controls.Add(this.YearBox);
+            this.Controls.Add(this.FuelBox);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.Model);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.Brand);
+            this.Name = "Form1";
+            this.Text = "CarDB";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewCars)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox Brand;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox Model;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ComboBox FuelBox;
+        private System.Windows.Forms.ComboBox YearBox;
+        private System.Windows.Forms.DataGridView dataGridViewCars;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button Add;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+using System.Data.SqlClient;
+using System.Configuration;
+using System.Xml.Linq;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace Form_Test_Fail
+{
+    public partial class Form1 : Form
+    {
+        string cs = ConfigurationManager.ConnectionStrings["dbCon"].ConnectionString;
+        SqlConnection con;
+        SqlCommand cmd;
+        SqlDataAdapter adapter;
+        DataTable dt;
+
+        // 0 references
+
+        public void ShowDataOnGridview()
+        {
+            using (con = new SqlConnection(cs))
+            {
+                adapter = new SqlDataAdapter("Select * From CarsTable", con);
+                dt = new DataTable();
+                adapter.Fill(dt);
+                dataGridViewCars.DataSource = dt;
+            }
+        }
+        public Form1()
+        {
+            InitializeComponent();
+            ShowDataOnGridview();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void dataGridViewCars_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            label6.Text = this.dataGridViewCars.CurrentRow.Cells["Brand"].Value.ToString();
+
+            Model.Text = this.dataGridViewCars.CurrentRow.Cells["Model"].Value.ToString();
+            YearBox.Text = this.dataGridViewCars.CurrentRow.Cells["Year"].Value.ToString();
+            FuelBox.Text = this.dataGridViewCars.CurrentRow.Cells["Fuel"].Value.ToString();            Brand.Text = this.dataGridViewCars.CurrentRow.Cells["Brand"].Value.ToString();
+            Brand.Text = this.dataGridViewCars.CurrentRow.Cells["VehicleBrand"].Value.ToString();
+        }
+
+        private void YearBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Model_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Add_Click(object sender, EventArgs e)
+        {
+            using (con = new SqlConnection(cs))
+            {
+                con.Open();
+                cmd = new SqlCommand("Insert Into CarsTable (Model, Year, Fuel, VehicleBrand) Values (@model, @year, @fuel, @vehiclebrand)", con);
+                cmd.Parameters.AddWithValue("@model", Model.Text);
+                cmd.Parameters.AddWithValue("@year", Model.Text);
+                cmd.Parameters.AddWithValue("@fuel", FuelBox.Text);
+                cmd.Parameters.AddWithValue("@vehiclebrand", Brand.Text);
+
+
+                cmd.ExecuteNonQuery();
+
+                con.Close();
+
+                MessageBox.Show("Data inserted successfully");
+                ShowDataOnGridview();
+
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            using (con = new SqlConnection(cs))
+            {
+                con.Open();
+
+                cmd = new SqlCommand("Delete From CarsTable Where Brand=@brand", con);
+                cmd.Parameters.AddWithValue("@brand", label6.Text);
+                cmd.ExecuteNonQuery();
+                con.Close();
+                ShowDataOnGridview();
+            }
+
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            using (con = new SqlConnection(cs))
+            {
+                con.Open();
+                cmd = new SqlCommand("UPDATE CarsTable Set Model=@model, Year=@year, Fuel=@fuel, VehicleBrand=@vehiclebrand Where Brand=@brand", con);
+                cmd.Parameters.AddWithValue("@model", Model.Text);
+                cmd.Parameters.AddWithValue("@year", YearBox.Text); // Changed from Model.Text to Year.Text
+                cmd.Parameters.AddWithValue("@fuel", FuelBox.Text);
+                cmd.Parameters.AddWithValue("@vehiclebrand", Brand.Text);
+                cmd.Parameters.AddWithValue("@brand", label6.Text);
+
+
+                cmd.ExecuteNonQuery();
+
+                con.Close();
+
+                MessageBox.Show("Data updated successfully");
+                ShowDataOnGridview();
+            }
+        }
+
+    }
+}
